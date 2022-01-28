f71354ab76c51f41c65e27be463ab57908628269
iamniklas <niklas.englmeier@web.de>
2022-01-28 01:38:45 +0100

    Updated to LIO-Core 0.0.7-1.10

--- src/main/java/SingleLEDRenderer.kt
+++ src/main/java/SingleLEDRenderer.kt
@@ -1,9 +1,11 @@
 import com.pi4j.io.gpio.GpioPinDigitalOutput
 import com.github.iamniklas.liocore.led.LEDRenderer
+import com.github.iamniklas.liocore.led.colorspace.LIOColor
+import java.util.ArrayList
 
-public class SingleLEDRenderer(private val pin: GpioPinDigitalOutput) : LEDRenderer(300) {
-    override fun render() {
-        if (colorData[0].r > 128 || colorData[0].g > 128 || colorData[0].b > 128) {
+public class SingleLEDRenderer(private val pin: GpioPinDigitalOutput) : LEDRenderer() {
+    override fun render(_colorData: ArrayList<LIOColor>?) {
+        if (_colorData!![0].r > 128 || _colorData[0].g > 128 || _colorData[0].b > 128) {
             pin.high()
         } else {
             pin.low()
